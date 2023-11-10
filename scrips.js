da745e08b9c55c8a7e2968976585ff5072b20790
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-11-11 01:38:16 +0200

    init

---
+++ scrips.js
@@ -0,0 +1,63 @@
+function generateGraph() {
+    var matrixInput = document.getElementById('matrixInput').value;
+    var adjacencyMatrix = parseMatrixInput(matrixInput);
+
+    var cy = cytoscape({
+        container: document.getElementById('cy'),
+        elements: GenerateElementsFromMatrix(adjacencyMatrix),
+        layout: { name: 'cose' },
+        style: [
+            {
+                selector: 'node',
+                style: 
+                {
+                    'background-color': '#520775',
+                    'label': 'data(node_count)',
+                    'border-width': 1,
+                    'border-color': '#33034a',
+                    'text-valign': 'center', 
+                    'text-halign': 'center', 
+                }
+            },
+            {
+                selector: 'edge',
+                style: 
+                {
+                    'width': 2,
+                    'line-color': '#000000',
+                    'curve-style': 'bezier',
+                    'target-arrow-shape': 'triangle',
+                    'target-arrow-color': '#000000'
+                }
+            }
+        ]
+    });
+}
+
+function parseMatrixInput(input) {
+    var rows = input.trim().split('\n');
+    var matrix = rows.map(row => row.trim().split(/\s+/).map(Number));
+    return matrix;
+}
+
+function GenerateElementsFromMatrix(matrix) {
+    var elements = [];
+
+    for (var i = 0; i < matrix.length; i++) {
+        elements.push({ data: { id: 'node' + (i + 1), node_count: i + 1 } });
+
+        for (var j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 1) {
+                elements.push({
+                    data: {
+                        id: 'edge' + i + j,
+                        source: 'node' + (i + 1),
+                        target: 'node' + (j + 1)
+                    }
+                });
+            }
+        }
+    }
+
+    return elements;
+}
